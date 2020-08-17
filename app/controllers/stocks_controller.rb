1e324ae5534cadf9e60d2e382c76e71ade468bf0
Luis089 <sirluis50@googlemail.com>
2020-08-17 15:42:07 +0200

    add ajax and JS

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,22 @@
 class StocksController < ApplicationController
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: "users/result" }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Symbol nicht bekannt, bitte valides Symbol eingeben"
+          format.js { render partial: "users/result" }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Bitte Symbol (Kürzel) eingeben, z.B. 'GOOGL' für Google."
+        format.js { render partial: "users/result" }
+      end
+    end
   end
 end
