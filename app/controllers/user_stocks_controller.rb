10cf8262acf5baf644d1f37e551ee938ce45e0e8
Luis089 <sirluis50@googlemail.com>
2020-08-18 17:00:02 +0200

    asdasdf

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,12 @@
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.check_db_for_ticker(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker]) #
+      stock.save
+    end
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Daten von #{stock.name} wurden zum Portfolio hinzugefügt."
+    redirect_to my_portfolio_path
+  end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
-  has_many :users, through: :user_stocks
+  has_many :users, through: :user_stocks, dependent: :destroy
 
   validates :name, :ticker, presence: true
   def self.new_lookup(ticker_symbol)
@@ -14,5 +14,8 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+  def self.check_db_for_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
