5c5b5301ac348b7d0ef95ee431faa84232d4cafd
Luis089 <sirluis50@googlemail.com>
2020-08-24 16:47:47 +0200

    user_stocks delete function and user infos added

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Daten von #{stock.name} wurden zum Portfolio hinzugefügt."
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.name} wurde von Liste entfernt."
+    redirect_to my_portfolio_path
+  end
 end
