111ed9019ebc258924e024d7ee364e870a9f9539
Luis089 <sirluis50@googlemail.com>
2020-08-31 13:17:03 +0200

    user search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
   def friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: "users/friend_result" }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Freund konnte nicht gefunden werden"
+          format.js { render partial: "users/friend_result" }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Bitte Namen oder E-Mail eines Freundes eingeben."
+        format.js { render partial: "users/friend_result" }
+      end
+    end
+  end
 end
